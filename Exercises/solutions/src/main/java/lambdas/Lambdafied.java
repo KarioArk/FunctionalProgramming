d14527042553f747a9cf3c91e5ea77746dcd1461
Haripriyan <haripriyan@live.com>
2018-04-15 17:37:30 +0530

    Solution for lambafying
    Former-commit-id: a74faae7b81a8ff11e6f932be72c31b872256ea6

--- Exercises/solutions/src/main/java/lambdas/Lambdafied.java
+++ Exercises/solutions/src/main/java/lambdas/Lambdafied.java
@@ -5,16 +5,54 @@ import java.util.function.Function;
 
 public class Lambdafied {
 
-    private interface Incrementor {
-        int increment(int x);
-    }
-
     public int increment(int x) {
-        return x + 1;
+        Function<Integer, Integer> incrementor = v -> v + 1;
+        return incrementor.apply(x);
     }
 
     public int add(int x, int y) {
-        return x + y;
+        BiFunction<Integer, Integer, Integer> adder = (v1, v2) -> v1 + v2;
+        return adder.apply(x, y);
     }
 
+    /** Intermediate solution 1 **/
+
+//    public int increment(int x) {
+//        Incrementor incrementor = new Incrementor() {
+//            @Override
+//            public int increment(int x) {
+//                return x + 1;
+//            }
+//        };
+//        return incrementor.increment(x);
+//    }
+//
+//    public int add(int x, int y) {
+//        Adder adder = new Adder() {
+//            @Override
+//            public int add(int x, int y) {
+//                return x + y;
+//            }
+//        };
+//        return adder.add(x, y);
+//    }
+
+    /** Intermediate solution 2 **/
+//    private interface Incrementor {
+//        int increment(int x);
+//    }
+//
+//    private interface Adder {
+//        int add(int x, int y);
+//    }
+//
+//    public int increment(int x) {
+//        Incrementor incrementor = v -> v + 1;
+//        return incrementor.increment(x);
+//    }
+//
+//    public int add(int x, int y) {
+//        Adder adder = (v1, v2) -> v1 + v2;
+//        return adder.add(x, y);
+//    }
 }
