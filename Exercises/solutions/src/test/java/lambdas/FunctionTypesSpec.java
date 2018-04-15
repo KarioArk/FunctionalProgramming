d3cb4cc88eaebe0044627d719c39b102a856c2e4
Haripriyan <haripriyan@live.com>
2018-04-15 20:53:45 +0530

    Add solution for recursive factorial
    Former-commit-id: 93e82514ed8509b69426c23f833826856327b455

--- Exercises/solutions/src/test/java/lambdas/FunctionTypesSpec.java
+++ Exercises/solutions/src/test/java/lambdas/FunctionTypesSpec.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 
 public class FunctionTypesSpec {
 
-    // TODO: This cannot be started till HOF are done
     // 1. Whats wrong here?
     @Test
     public void useRunnerForGreetingTheUser5Times() {
@@ -12,10 +11,11 @@ public class FunctionTypesSpec {
     }
 
     // 2. Use the greeter to greet the user 5 times a long with the index
-    // 3. Find the square of 42
-    // 4. Find the cube of 7
-    // 5. Find the value of 5 raised to the power of 5
-    // 6. Express square and cube in terms of power function
+    // 3. Write a simple count down timer which prints the remaining time
+    // 4. Recursive factorial, fibonacci
+    // 5. Find the square of 42
+    // 6. Find the cube of 7
+    // 7. Find the value of 5 raised to the power of 5
+    // 8. Express square and cube in terms of power function
 
-    // TODO: Recursive fibonacci, sum, factorial
 }
