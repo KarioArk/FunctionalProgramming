0fb0085bb3ef91a6bfb87b12c97a68b8a26d9d09
arunmadhavan-g <arunmadhavan.g@gmail.com>
2018-04-16 22:40:46 +0530

    Added Initial solution of Super Hero and Builder PAtterns and comparisons
    Former-commit-id: 5b8bc2c0e9ec67f3f658e7885c0387fba9a7992f

--- Solutions/src/main/java/common/superheros/SuperHero.java
+++ Solutions/src/main/java/common/superheros/SuperHero.java
@@ -3,50 +3,76 @@ package common.superheros;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SuperHero {
+public class SuperHero implements  Comparable<SuperHero> {
 
     private final String name;
     private final int strength;
-    private final String fightResult;
-
-    private SuperHero(String name, int strength, String lastFightResult) {
-        this.name = name;
-        this.strength = strength;
-        this.fightResult = lastFightResult;
-    }
+    private final String realName;
+    private final boolean fromEarth;
+    private final List<String> associates;
 
     public SuperHero(String name, int strength) {
-        this(name, strength, "");
+        this(name, strength, "", false, new ArrayList<>());
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public SuperHero fight(SuperHero opponent) {
-        if(opponent.strength > this.strength) {
-            return new SuperHero(this.name, this.strength, "Lose");
-        } else if(opponent.strength < this.strength) {
-            return new SuperHero(this.name, this.strength + 2, "Win");
-        }
-        return new SuperHero(this.name, this.strength + 1, "Draw");
-    }
-
-    public String fightResult() {
-        return this.fightResult;
-    }
-
-    public int currentStrength() {
-        return this.strength;
-    }
-
-    // TODO: To be implemented
-    public String fightHistory() {
-        return "";
+    private SuperHero(String name, int strength, String realName, boolean fromEarth, List<String> associates) {
+        this.name = name;
+        this.strength = strength;
+        this.realName = realName;
+        this.fromEarth = fromEarth;
+        this.associates = associates;
     }
 
     @Override
     public String toString() {
-        return this.name;
+        return String.format("%s#%d", name, strength);
+    }
+
+    @Override
+    public int compareTo(SuperHero opponent) {
+        return this.strength - opponent.strength ;
+    }
+
+    public static class Builder {
+
+        private String name;
+        private int strength;
+        private String realName;
+        private boolean fromEarth;
+        private List<String> associates = new ArrayList<>();
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder strength(int level) {
+            this.strength = level;
+            return this;
+        }
+
+        public Builder realName(String realName) {
+            this.realName = realName;
+            return this;
+        }
+
+        public Builder fromEarth(boolean isFromEarth) {
+            this.fromEarth = isFromEarth;
+            return this;
+        }
+
+        public Builder addAssociates(String associate) {
+            this.associates.add(associate);
+            return this;
+        }
+
+        public SuperHero build() {
+            return new SuperHero(this.name,
+                                this.strength,
+                                this.realName,
+                                this.fromEarth,
+                                this.associates);
+        }
+
     }
 }
