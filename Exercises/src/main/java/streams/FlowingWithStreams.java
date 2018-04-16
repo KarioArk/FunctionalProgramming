9eed2175dca87ba1d9899d853873a5acedbb9a73
Haripriyan <haripriyan@live.com>
2018-04-16 22:05:29 +0530

    Exercises streams (#17)
    * Add few exercises
    
    * Streams exercises
    
    
    Former-commit-id: d5810497979cb9604499aea6b295baea603e5a5a

--- Exercises/src/main/java/streams/FlowingWithStreams.java
+++ Exercises/src/main/java/streams/FlowingWithStreams.java
@@ -1,7 +1,10 @@
 package streams;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class FlowingWithStreams {
 
@@ -16,4 +19,23 @@ public class FlowingWithStreams {
         return null;
     }
 
+    public void expensiveOp() {
+        timer(Arrays.asList(1,2,3,4,5,6).stream());
+    }
+
+    private void timer(Stream<Integer> stream) {
+        long startTime = System.nanoTime();
+        stream.map(x->expensiveSquare(x)).collect(Collectors.toList());
+        long timetaken = System.nanoTime()-startTime;
+        System.out.println("Time taken "+timetaken/1000 + " nano seconds");
+    }
+
+    private int expensiveSquare(int x){
+        try{
+            Thread.sleep(x*100);
+        } catch (Exception e){ }
+        int square = x*x;
+        System.out.println("Square called. Result:"+square);
+        return square;
+    }
 }
--- Exercises/src/test/java/streams/FlowingWithStreamsSpec.java
+++ Exercises/src/test/java/streams/FlowingWithStreamsSpec.java
@@ -11,13 +11,22 @@ public class FlowingWithStreamsSpec {
     public void shouldGenerateNaturals() {
         assertThat(flowingWithStreams.firstNaturals(10).toString()).isEqualTo("[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]");
     }
-    // 2. Generate N random numbers
-    // 3. Generate the sets of pythogoreans within 10.
+
+    // 2. Try out Parallel streams
+    @Test
+    public void shouldTryOutParallelStreams() {
+        flowingWithStreams.expensiveOp();
+//        flowingWithStreams.expensiveOpUsingParallel();
+    }
+
+    // 3. Generate N random numbers
+    // 4. Generate the sets of pythogoreans within 10.
     // X,Y,Z are said to be pythogorean when X^2 + Y^2 = Z^2
     @Test
     public void shouldFirstNPythogoreans() {
         assertThat(flowingWithStreams.pythoGoreans(10).toString()).isEqualTo("[[3, 4, 5], [4, 3, 5], [6, 8, 10], [8, 6, 10]]");
     }
 
-    // 4. Use streams to solve factorials fibonacci series
+    // 5. Use streams to solve factorials fibonacci series
+
 }
