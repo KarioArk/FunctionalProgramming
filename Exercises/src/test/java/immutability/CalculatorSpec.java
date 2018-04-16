02aae649f719aebe62494926189ac87ddaccecbf
arunmadhavan-g <arunmadhavan.g@gmail.com>
2018-04-16 20:23:54 +0530

    Added code for memory calculator solution
    Former-commit-id: e86c1377709e56ec585432f0f7ac969730c5326c

--- Exercises/src/test/java/immutability/CalculatorSpec.java
+++ Exercises/src/test/java/immutability/CalculatorSpec.java
@@ -1,33 +1,30 @@
 package immutability;
 
 import org.junit.Test;
-import static org.assertj.core.api.Assertions.*;
 
 public class CalculatorSpec {
 
-    private Calculator calculator = new Calculator();
-
-
     @Test
     public void addTwoNumbersReturnsTheSum(){
-        assertThat(calculator.add(3, 4)).isEqualTo(7);
-        assertThat(calculator.add(9, 10)).isEqualTo(19);
-        assertThat(calculator.add(3, 4)).isEqualTo(7);
-        assertThat(calculator.add(2, 3)).isEqualTo(5);
     }
 
     @Test
     public void ableToAddTwoNumbersWithMemory() {
-        assertThat(calculator.addWithMemory(2)).isEqualTo(2);
-        assertThat(calculator.addWithMemory(5)).isEqualTo(7);
-        assertThat(calculator.addWithMemory(7)).isEqualTo(14);
-        assertThat(calculator.addWithMemory(2)).isEqualTo(16);
+
     }
 
+    // Implement the same with immutable objects
+    @Test
+    public void ableToTwoAddNumbersUsingImmutableCalculator() {
 
-    /**
-     * 1. Reimplement the above with immutable Calculator
-     **/
+    }
 
+    @Test
+    public void ableToAddNumbersWithMemory() {
+    }
 
+    @Test
+    public void ableToReplayHistoryOfCalculations() {
+
+    }
 }
